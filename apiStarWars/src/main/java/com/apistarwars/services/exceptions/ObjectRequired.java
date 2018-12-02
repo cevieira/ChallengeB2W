b4cf75463a16f4cef121b8ac5a27ec133ed5cb99
Carlos Eduardo Vieira <ce.vieira98@gmail.com>
2018-12-02 18:48:16 -0200

    Refatorando

--- apiStarWars/src/main/java/com/apistarwars/services/exceptions/ObjectRequired.java
+++
@@ -1,8 +0,0 @@
-package com.apistarwars.services.exceptions;
-
-public class ObjectRequired extends RuntimeException {
-
-    public ObjectRequired(String msg){
-        super(msg);
-    }
-}
---
+++ src/main/java/com/apistarwars/apiStarWarsApplication.java
@@ -0,0 +1,14 @@
+package com.apistarwars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan
+public class apiStarWarsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(apiStarWarsApplication.class, args);
+    }
+}
